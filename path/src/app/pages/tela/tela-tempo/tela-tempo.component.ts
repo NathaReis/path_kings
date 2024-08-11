dee191cd110189f953257210d1143d5bd00553e7
Reis <nathan.david@siriusgroup.com.br>
2024-08-11 07:21:44 -0300

    Temporizador finalizado

--- path/src/app/pages/tela/tela-tempo/tela-tempo.component.ts
+++ path/src/app/pages/tela/tela-tempo/tela-tempo.component.ts
@@ -14,7 +14,10 @@ export class TelaTempoComponent implements OnInit {
   clima: Clima = {src: '', cep: '', temperatura: '', descricao: ''};
   hora: string = '00';
   minuto: string = '00';
-  segundo: number = 0;
+  segundo: string = '00';
+  seg: number = 0;
+  primeiraVez: boolean = true;
+  pararContagem: boolean = false;
 
   constructor(readonly geo: GeoService, private route: ActivatedRoute) { }
 
@@ -24,6 +27,8 @@ export class TelaTempoComponent implements OnInit {
     this.route.params.subscribe((params: any) => {
       this.id = String(params["id"]);
     });// Busca id
+
+    this.buscarRegistro();
   }
 
   async buscarClima(): Promise<void> {
@@ -50,49 +55,80 @@ export class TelaTempoComponent implements OnInit {
     this.minuto = '00';
   }
 
-  registrarTempo(hora: string, minuto: string): void {
+  registrarTempo(hora: string, minuto: string, segundo: string): void {
     sessionStorage.setItem(`tempo_${this.id}`,`${hora}:${minuto}`);
+    sessionStorage.setItem(`segundo_${this.id}`,segundo);
   }
 
-  preencherTempo(hora: string, minuto: string): void {
+  preencherTempo(hora: string, minuto: string, segundo: string): void {
     this.hora = hora;
     this.minuto = minuto;
+    this.segundo = segundo;
   }
 
-  renderizarTempo(hora: string, minuto: string): void {
-    this.registrarTempo(this.formatarHora(hora),this.formatarHora(minuto));
-    this.preencherTempo(this.formatarHora(hora),this.formatarHora(minuto));
-    console.log(hora, ':', minuto, ':', this.segundo);
+  renderizarTempo(hora: string, minuto: string, segundo: string): void {
+    this.registrarTempo(this.formatarHora(hora),this.formatarHora(minuto),this.formatarHora(segundo));
+    this.preencherTempo(this.formatarHora(hora),this.formatarHora(minuto),this.formatarHora(segundo));
+  }
+
+  resetContagem() {
+    this.pararContagem = true;
+    this.seg = 0;
+    this.segundo = '00';
+    this.minuto = '00';
+    this.hora = '00';
   }
 
   contar(): void {
-    this.segundo--;
-    if(this.segundo < 0) {
+    this.seg = +this.segundo - 1;
+    if(this.seg < 0) {
       if(+this.minuto > 0) {
-        this.segundo = 59;
+        this.seg = 59;
         const decremento: number = +this.minuto - 1;
         this.minuto = this.formatarHora(String(decremento));
       }
       else if(+this.hora > 0) {
-        this.segundo = 59;
+        this.seg = 59;
         this.minuto = '59';
         const decremento: number = +this.hora - 1;
         this.hora = this.formatarHora(String(decremento));
       }
+      else {
+        this.resetContagem();
+      }
     }
-    this.renderizarTempo(this.hora,this.minuto);
+    this.segundo = this.formatarHora(String(this.seg));
+
+    this.renderizarTempo(this.hora,this.minuto,this.segundo);
   }
 
-  iniciarContagem(tempo: string): void {
-    let contagem; // Defino a contagem
+  buscarRegistro(): void {
+    setTimeout(() => {
+      const tempo = sessionStorage.getItem(`tempo_${this.id}`);
+      const segundo = sessionStorage.getItem(`segundo_${this.id}`);
+      const semLocalStorage = this.hora === '00' && this.minuto === '00';
+      if(!!tempo && !!segundo && semLocalStorage && this.primeiraVez) {
+        this.iniciarContagem(tempo,segundo);
+      }
+    },500); // Esperar comando do LocalStorage
+  }
+
+  iniciarContagem(tempo: string, segundo?: string): void {
     const hora = tempo.split(":")[0]; // Recupero os dados recebidos
     const minuto = tempo.split(":")[1];
+    this.segundo = segundo ? segundo : '00';
+    this.seg = segundo ? +segundo : 0;
+    this.pararContagem = false;
 
-    clearInterval(contagem); // Paro a contagem atual
-    this.renderizarTempo(hora,minuto); // Registro do tempo
+    this.renderizarTempo(hora,minuto,this.segundo); // Registro do tempo
 
-    contagem = setInterval(() => {
-      this.contar();
-    }, 1000); // Inicio a contagem
+    if(this.primeiraVez) {
+      this.primeiraVez = false;
+      setInterval(() => {
+        if(!this.pararContagem) {
+          this.contar();
+        }
+      }, 1000); // Inicio a contagem
+    }
   }
 }
