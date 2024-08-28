e4fb1cd67cd3d3753b3eefe354d1599de0997e46
NathaReis <153759333+NathaReis@users.noreply.github.com>
2024-08-28 14:22:30 -0300

    editando a midia

--- path/src/app/pages/casa/media/media.component.ts
+++ path/src/app/pages/casa/media/media.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatSelectionListChange } from '@angular/material/list';
 
 import { Media } from 'src/app/models/Media';
 
@@ -10,12 +11,23 @@ import { Media } from 'src/app/models/Media';
 export class MediaComponent {
   listaAtual: string = 'video';
 
-  videos: Media[] = [{nome: 'Vídeo Teste', dados: ''}];
+  videos: Media[] = [{nome: 'Vídeo Teste', dados: ''},{nome: 'Vídeo Teste', dados: ''},{nome: 'Vídeo Teste', dados: ''}];
   imagens: Media[] = [{nome: 'Imagem Teste', dados: ''}];
   audios: Media[] = [{nome: 'Audio Teste', dados: ''}];
   powerpoints: Media[] = [{nome: 'PowerPoint Teste', dados: ''}];
 
   navMidia(tipo: string): void {
     this.listaAtual = tipo;
+  }
+
+  onSelectionChange(event: MatSelectionListChange) {
+    if (event.source.selectedOptions.selected.length > 0) {
+      const selectedVideo = event.source.selectedOptions.selected[0];
+      console.log('Vídeo selecionado:', selectedVideo);
+
+      // Faça algo com o vídeo selecionado, como:
+      // - Chamar um serviço para buscar mais informações
+      // - Atualizar outra parte da interface
+    }
   }
 }
